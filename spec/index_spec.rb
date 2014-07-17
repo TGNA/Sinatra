bb59f016ae3e90edf0cf7d0e7d4ada22559520ee
Oscar <oscarbc@me.com>
2014-07-17 15:47:16 +0200

    commit

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -8,8 +8,8 @@ describe "Film" do
 
  it "first have poster" do
    imdb_searcher = double(['hola', nil])
-   imdb_searcher.stub(:search_movie).and_return('poster')
-   expect(imdb_searcher.search_movie).not_to eq(nil)
+   imdb_searcher.stub(:search_movie).and_return(['hola', nil])
+   expect(imdb_searcher.search_movie).not_to eq(['hola'])
  end
 
 end
